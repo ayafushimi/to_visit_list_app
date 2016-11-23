582bd9b1eed4f784a0775c2024193e2ece3e0d91
ayafushimi <aya.fushimi@gmail.com>
2016-11-23 12:42:35 -0500

    modified seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,17 +2,19 @@ User.create(username:"aaa", password:"aaa")
 User.create(username:"bbb", password:"bbb")
 
 countries_list = {
-    "Algeria" => ["Africa", User.find(1)],
-    "Cameroon" => ["Africa", User.find(1)],
-    "Bangladesh" => ["Asia", User.find(1)],
-    "Finland" => ["Europe", User.find(2)]
+    "Algeria" => {region:"Africa", user_id:1},
+    "Cameroon" => {region:"Africa", user_id:1},
+    "Bangladesh" => {region:"Asia", user_id:1},
+    "Finland" => {region:"Europe", user_id:2}
   }
 
-countries_list.each do |name, arr|
-  p = Country.new(name: name, region: arr[0])
-  p.users << arr[1]
+countries_list.each do |name, hash|
+  p = Country.new
+  p.name = name
+  hash.each do |attribute, value|
+      p[attribute] = value
+  end
   p.save
-  arr[1].save
 end
 
 cities_list = {
