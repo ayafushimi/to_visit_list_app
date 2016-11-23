af3b6523167b1b94a63189348859dfdf33a929f2
ayafushimi <aya.fushimi@gmail.com>
2016-11-23 13:47:13 -0500

    removed association between user and city, remigrated and seeded

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,4 +1,3 @@
 class City < ActiveRecord::Base
   belongs_to :country
-  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base
   has_many :countries
-  has_many :cities, through: :countries
   has_secure_password
 end
