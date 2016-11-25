88f1654a0e0eb45b3c303b821cfe084439e3c088
ayafushimi <aya.fushimi@gmail.com>
2016-11-25 10:31:34 -0500

    added @country_name_input @region_  to keep input value when error

--- app/controllers/city_controller.rb
+++ app/controllers/city_controller.rb
@@ -64,19 +64,6 @@ class CityController < ApplicationController
         flash.now[:create_errors] << "Please select 'Country'"
         flash.now[:country_error] = "has-error"
       end
-      if create_country
-        if empty_country_name
-          flash.now[:create_errors] << "Please enter 'Country Name'"
-          flash.now[:country_name_error] = "has-error"
-        elsif exist_country
-          flash.now[:create_errors] << "This Country already exists. Please select from the avobe list"
-          flash.now[:country_name_error] = "has-error"
-        end
-        if empty_region
-          flash.now[:create_errors] << "Please select 'Region'"
-          flash.now[:country_region_error] = "has-error"
-        end
-      end
 
       @name_input = params[:name]
       case params[:rank]
@@ -99,20 +86,33 @@ class CityController < ApplicationController
         end
       end
 
-    #   case params[:country][:region]
-    #   when "Africa"
-    #     @region_africa = "selected"
-    #   when "Americas"
-    #     @region_america = "selected"
-    #   when "Asia"
-    #     @region_asia = "selected"
-    #   when "Europe"
-    #     @region_europe = "selected"
-    #   when "Oceania"
-    #     @region_oceania = "selected"
-    #   end
-    #   @city_name_input = params[:city][:name]
-    #   erb :"/countries/create"
+      if create_country
+        if empty_country_name
+          flash.now[:create_errors] << "Please enter 'Country Name'"
+          flash.now[:country_name_error] = "has-error"
+        elsif exist_country
+          flash.now[:create_errors] << "This Country already exists. Please select from the avobe list"
+          flash.now[:country_name_error] = "has-error"
+        end
+        if empty_region
+          flash.now[:create_errors] << "Please select 'Region'"
+          flash.now[:country_region_error] = "has-error"
+        end
+        @country_name_input = params[:country][:name]
+        case params[:country][:region]
+        when "Africa"
+          @region_africa = "selected"
+        when "Americas"
+          @region_america = "selected"
+        when "Asia"
+          @region_asia = "selected"
+        when "Europe"
+          @region_europe = "selected"
+        when "Oceania"
+          @region_oceania = "selected"
+        end
+      end
+      erb :"/cities/create"
     end
   end
 
