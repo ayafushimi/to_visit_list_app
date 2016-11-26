8663e88f9981584cd63578ce08bfcb409e2ee3d5
ayafushimi <aya.fushimi@gmail.com>
2016-11-25 20:22:33 -0500

    defined post /cities/:id/edit

--- app/controllers/city_controller.rb
+++ app/controllers/city_controller.rb
@@ -148,4 +148,103 @@ class CityController < ApplicationController
     end
   end
 
+  post "/cities/:id/edit" do
+    @city = City.find(params[:id])
+
+    empty_name = params[:name].empty?
+    exist_city = City.all.detect{|x| x.name == params[:name]}
+    empty_rank = params[:rank].empty?
+    empty_country = params[:country_id].empty?
+    create_country = (params[:country_id] == "create_country")
+    if create_country
+      empty_country_name = params[:country][:name].empty?
+      exist_country = Country.all.detect{|x| x.name == params[:country][:name]}
+      empty_region =  params[:country][:region].empty?
+    end
+
+    if !(empty_name||exist_city||empty_rank||empty_country||empty_country_name||exist_country||empty_region)
+      @city.update(name:params[:name], rank:params[:rank])
+      if !create_country
+        @city.update(country_id:params[:country_id])
+      else
+        country = Country.create(params[:country])
+        country.update(user_id:current_user.id)
+        @city.update(country_id:country.id)
+      end
+      redirect "/cities/#{@city.id}"
+    else
+      flash.now[:edit_errors] = []
+      if empty_name
+        flash.now[:edit_errors] << "Please enter 'City Name'"
+        flash.now[:name_error] = "has-error"
+      elsif exist_city
+        flash.now[:edit_errors] << "This City already exists. Please check <a href='/cities/#{exist_city.id}' class='alert-link'>this page</a>"
+        flash.now[:name_error] = "has-error"
+      end
+      if empty_rank
+        flash.now[:edit_errors] << "Please select 'Rank'"
+        flash.now[:rank_error] = "has-error"
+      end
+      if empty_country
+        flash.now[:edit_errors] << "Please select 'Country'"
+        flash.now[:country_error] = "has-error"
+      end
+
+      @name_input = params[:name]
+      case params[:rank]
+      when "5"
+        @rank5 = "selected"
+      when "4"
+        @rank4 = "selected"
+      when "3"
+        @rank3 = "selected"
+      when "2"
+        @rank2 = "selected"
+      when "1"
+        @rank1 = "selected"
+      end
+      current_user.countries.each do |country|
+        if country.id == params[:country_id].to_i
+          var = "@country_#{country.id}"
+          value = "selected"
+          eval("#{var} = value")
+        end
+      end
+      @country_name_disabled = "disabled"
+      @country_region_disabled = "disabled"
+
+      if create_country
+        if empty_country_name
+          flash.now[:create_errors] << "Please enter 'Country Name'"
+          flash.now[:country_name_error] = "has-error"
+        elsif exist_country
+          flash.now[:create_errors] << "This Country already exists. Please select from the avobe list"
+          flash.now[:country_name_error] = "has-error"
+        end
+        if empty_region
+          flash.now[:create_errors] << "Please select 'Region'"
+          flash.now[:country_region_error] = "has-error"
+        end
+        @create_country = "selected"
+        @country_name_disabled = ""
+        @country_region_disabled = ""
+        @country_name_input = params[:country][:name]
+        case params[:country][:region]
+        when "Africa"
+          @region_africa = "selected"
+        when "Americas"
+          @region_america = "selected"
+        when "Asia"
+          @region_asia = "selected"
+        when "Europe"
+          @region_europe = "selected"
+        when "Oceania"
+          @region_oceania = "selected"
+        end
+      end
+      erb :"/cities/edit"
+    end
+  end
+
+
 end
