4e83f80b20b79a0d03b4726d28bcbf7a3d61e6c8
ayafushimi <aya.fushimi@gmail.com>
2016-11-28 10:03:59 -0500

    defined #cities in User class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ActiveRecord::Base
   has_many :countries
   has_secure_password
+
+  def cities
+    cities = []
+    self.countries.each do |country|
+      country.cities do |city|
+        cities << city
+      end
+    end
+    cities
+  end
+
 end
